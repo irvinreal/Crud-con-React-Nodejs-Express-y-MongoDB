a4d45afff2bbf5aabe325ca0b8df182a82f52e4c
irvinreal <irvin.glez.real@hotmail.com>
2024-02-09 18:44:00 -0600

    Cambiando el backend a otro sitio

--- api/src/index.js
+++
@@ -1,9 +0,0 @@
-import app from './app.js';
-import { connectDB } from './db.js';
-const { PORT } = process.env;
-
-connectDB();
-
-app.listen(PORT, () => {
-  console.log('server on port', PORT);
-});
